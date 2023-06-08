44116d732a27459e88840eaa9ed1a5a653d21dc6
alijawadfahs <alijawadfahs@gmail.com>
2023-06-08 12:33:20 +0200

    fix the same sg from two different clouds

--- src/database/db.py
+++ src/database/db.py
@@ -23,36 +23,36 @@ def load_clouds(file=DIR+"/Clouds.json"):
 	logging.info("Loaded clouds table")
 	return db 
 
-def load_servers(file=DIR+"/Servers.json"): 
+def load_servers(file=DIR+"/Servers.json"):
 	db = TinyDB(file)
 	logging.info("Loaded servers table")
 	return db
 
-def load_security_groups(file=DIR+"/Security_Groups.json"): 
+def load_security_groups(file=DIR+"/Security_Groups.json"):
 	db = TinyDB(file)
 	logging.info("Loaded servers table")
 	return db
 
-def load_tokens(file=DIR+"/Tokens.json"): 
+def load_tokens(file=DIR+"/Tokens.json"):
 	db = TinyDB(file)
 	logging.info("Loaded tokens table")
 	return db
 
-def load_cluster(file=DIR+"/Cluster.json"): 
+def load_cluster(file=DIR+"/Cluster.json"):
 	db = TinyDB(file)
 	logging.info("Loaded cluster table")
 	return db 
 
-def add_cloud_item(cloud_item): 
-	cloud=load_clouds()
-	cloud_table=cloud.table("Clouds")
-	out=cloud_table.insert(cloud_item)
+def add_cloud_item(cloud_item):
+	cloud		= load_clouds()
+	cloud_table	= cloud.table("Clouds")
+	out			= cloud_table.insert(cloud_item)
 	return out
 
 def add_server_item(server_item):
-	servers=load_servers()
-	servers_table=servers.table("Servers")
-	out=servers_table.insert(server_item)
+	servers			= load_servers()
+	servers_table	= servers.table("Servers")
+	out				= servers_table.insert(server_item)
 	return out
 
 def add_ip_item(ip_item):
@@ -62,63 +62,81 @@ def add_ip_item(ip_item):
 	return out
 
 def add_cluster_sg_item(cluster_sg_item):
-	cluster=load_cluster()
-	SG_table=cluster.table("SG")
-	out=SG_table.insert(cluster_sg_item)
+	cluster		= load_cluster()
+	SG_table	= cluster.table("SG")
+	out			= SG_table.insert(cluster_sg_item)
 	return out
 
 def add_sg_item(sg_item):
-	security_groups=load_security_groups()
-	security_groups_table=security_groups.table("Security_Groups")
-	out=security_groups_table.insert(sg_item)
+	security_groups			= load_security_groups()
+	security_groups_table	= security_groups.table("Security_Groups")
+	out						= security_groups_table.insert(sg_item)
 	return out
 
 def get_sg_ids():
-	security_groups=load_security_groups()
-	security_groups_table=security_groups.table("Security_Groups")
-	out=[sg["id"] for sg in security_groups_table.all()]
+	security_groups			= load_security_groups()
+	security_groups_table	= security_groups.table("Security_Groups")
+	out						= [sg["id"] for sg in security_groups_table.all()]
 	return out
 
 def get_server_ids():
-	servers=load_servers()
-	servers_table=servers.table("Servers")
-	out=[server["id"] for server in servers_table.all()]
+	servers			= load_servers()
+	servers_table	= servers.table("Servers")
+	out				= [server["id"] for server in servers_table.all()]
 	return out
 
 def add_kubernetes_token(token):
-	tokens=load_tokens()
-	tokens_table=tokens.table("Tokens")
-	out = tokens_table.insert({"token" : token})
+	tokens			= load_tokens()
+	tokens_table	= tokens.table("Tokens")
+	out				= tokens_table.insert({"token" : token})
 	return out
 
-def get_security_group(sg_name): 
+def get_security_group(sg_name):
+	security_groups			= load_security_groups()
+	security_groups_table	= security_groups.table("Security_Groups")
+	query 					= Query()
+	return security_groups_table.search(query.name == sg_name)
+
+def get_security_group_aws(sg_id, provider_name):
+	security_groups			= load_security_groups()
+	security_groups_table	= security_groups.table("Security_Groups")
+	query 					= Query()
+	return security_groups_table.search(
+		(query.id 				== sg_id) &
+		(query.provider_name 	== provider_name) &
+		(query.provider_type 	== "aws"))
+
+def get_security_group_openstack(sg_name, provider_name):
 	security_groups=load_security_groups()
 	security_groups_table=security_groups.table("Security_Groups")
 	query = Query()
-	return security_groups_table.search(query.name == sg_name)
+	return security_groups_table.search(
+		(query.name 			== sg_name) &
+		(query.provider_name 	== provider_name) &
+		(query.provider_type 	== "openstack"))
 
-def get_kubernetes_token(token_id): 
-	kubernetes_tokens=load_tokens()
-	kubernetes_tokens_table=kubernetes_tokens.table("Tokens")
+def get_kubernetes_token(token_id):
+	kubernetes_tokens		= load_tokens()
+	kubernetes_tokens_table	= kubernetes_tokens.table("Tokens")
 	return kubernetes_tokens_table.get(doc_id=token_id)
 
-def clear_database(): 
+def clear_database():
 	for file in FILES:
-		to_be_deleted= f"{DIR}/{file}"
+		to_be_deleted = f"{DIR}/{file}"
 		if os.path.exists(to_be_deleted):
 			os.remove(to_be_deleted)
 			
 def clear_sg_db():
-	file="Security_Groups.json"
-	to_be_deleted= f"{DIR}/{file}"
+	file			= "Security_Groups.json"
+	to_be_deleted	= f"{DIR}/{file}"
 	if os.path.exists(to_be_deleted):
 			os.remove(to_be_deleted)
 	load_security_groups()
 			
-def get_server_by_ip(ip): 
-	servers=load_servers()
-	servers_table=servers.table("Servers")
-	query = Query()
+def get_server_by_ip(ip):
+	servers			= load_servers()
+	servers_table	= servers.table("Servers")
+	query 			= Query()
 	return servers_table.search(query.address == ip)
 
 def add_ip_to_cluster(server):
@@ -130,41 +148,55 @@ def add_ip_to_cluster(server):
 
 
 def add_sg_to_cluster(server, sg):
-	sg_item={}
-	sg_item["name"]=sg["name"]
-	sg_item["id"]=sg["id"]
-	sg_item["provider_name"]=sg["provider_name"]
-	sg_item["provider_type"]=sg["provider_type"]
-	sg_item["server"]=server["name"]
+	sg_item						= {}
+	sg_item["name"]				= sg["name"]
+	sg_item["id"]				= sg["id"]
+	sg_item["provider_name"]	= sg["provider_name"]
+	sg_item["provider_type"]	= sg["provider_type"]
+	sg_item["server"]			= server["name"]
 	add_cluster_sg_item(sg_item)
 
 
-def get_server_sg(server): 
-	sg_name=server["security_groups"][0]
-	sg_query=get_security_group(sg_name)
-	if not sg_query: 
-		raise Exception("The security group with name \"%s\" was not found!" % sg_name )
-		return
-	elif len(sg_query)>1:
-		raise Exception("OpenStack chaos! two security groups with the same name \"%s\"" % sg_name )
-		return
-	else:
-		sg=sg_query[0]
-		return sg
+def get_server_sg(server):
+	match server["provider_type"]:
+		case "openstack":
+			sg_name		= server["security_groups"][0]
+			sg_query 	= get_security_group_openstack(sg_name, server["provider_name"])
+			if not sg_query:
+				raise Exception("The security group with name \"%s\" was not found!" % sg_name )
+				return
+			elif len(sg_query)>1:
+				raise Exception("OpenStack chaos! two security groups with the same name \"%s\"" % sg_name )
+				return
+			else:
+				sg = sg_query[0]
+				return sg
+		case "aws":
+			sg_id		= server["security_groups_ids"][0]
+			sg_query	= get_security_group_aws(sg_id, server["provider_name"])
+			if not sg_query:
+				raise Exception("The security group with id \"%s\" was not found!" % sg_id )
+				return
+			elif len(sg_query)>1:
+				raise Exception("AWS chaos! two security groups with the same id \"%s\"" % sg_id )
+				return
+			else:
+				sg = sg_query[0]
+				return sg
 
 def get_cluster_ips(): 
-	cluster=load_cluster()
-	ips_table=cluster.table("Ips")
-	ips=set()
+	cluster		= load_cluster()
+	ips_table	= cluster.table("Ips")
+	ips			= set()
 	for ip in ips_table.all():
 		ips.add(ip["ip"])
 	return ips
 
 
 def get_cluster_sgs(): 
-	cluster=load_cluster()
-	SG_table=cluster.table("SG")
-	SGs=[]
+	cluster		= load_cluster()
+	SG_table	= cluster.table("SG")
+	SGs			= []
 	for sg in SG_table.all():
 		SGs.append({"provider_name":sg["provider_name"],"provider_type":sg["provider_type"],"id":sg["id"]})
 	return SGs
